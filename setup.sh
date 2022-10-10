2c43da44f47f0b695f3725e0767c08dabc3977de
ameenahb <ameenahb@google.com>
2022-10-10 10:47:36 -0400

    added setup.sh to deploy online boutique as sample app

---
+++ setup.sh
@@ -0,0 +1,43 @@
+gcloud services enable \
+    compute.googleapis.com \
+    container.googleapis.com \
+    monitoring.googleapis.com \
+    artifactregistry.googleapis.com \
+    cloudbuild.googleapis.com \
+    cloudscheduler.googleapis.com \
+    eventarc.googleapis.com \
+    run.googleapis.com \
+    cloudfunctions.googleapis.com
+
+
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+
+gcloud container clusters create online-boutique \
+    --project=${PROJECT_ID} --zone=${ZONE} \
+    --machine-type=e2-standard-2 --num-nodes=4
+
+echo "waiting for cluster"
+
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
+
+echo "To simulate a more realistic environment, create an HPA for Online Boutique deployments"
+kubectl get deployments --field-selector='metadata.name!=adservice,metadata.name!=cartservice' -o go-template-file=k8s/templates/cpu-hpa.gtpl | kubectl apply -f -
+
+kubectl get deployments --field-selector='metadata.name==cartservice' -o go-template-file=k8s/templates/memory-hpa.gtpl | kubectl apply -f -
+
+kubectl get hpa
+
+echo "Building the custom metric exporter image"
+docker build . -t gcr.io/$PROJECT_ID/metrics-exporter
+
+echo "Pushing the custom metric exporter image"
+sleep 30s
+docker push gcr.io/$PROJECT_ID/metrics-exporter
+
+sleep 30s
+sed "s/PROJECT_ID/$PROJECT_ID/g" ./k8s/templates/hpa-metrics-exporter.yaml > ./k8s/metrics-exporter.yaml
+kubectl create ns custom-metrics
+kubectl apply -f k8s/metrics-exporter.yaml
+
+echo "SETUP COMPLETE"
