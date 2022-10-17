5b826c4ae1a74132543466a5fe5d49bc326ff4f8
Ameenah <ameenahb@google.com>
2022-10-17 22:06:39 +0000

    removed unused apis

--- setup.sh
+++ setup.sh
@@ -2,13 +2,7 @@ echo "Setup starting, enable services please wait..."
 gcloud services enable \
     compute.googleapis.com \
     container.googleapis.com \
-    monitoring.googleapis.com \
-    #artifactregistry.googleapis.com \
-    cloudbuild.googleapis.com \
-    cloudscheduler.googleapis.com \
-    #eventarc.googleapis.com \
-    #run.googleapis.com \
-    cloudfunctions.googleapis.com
+    monitoring.googleapis.com
 
 echo "Configuring region and zone"
 
